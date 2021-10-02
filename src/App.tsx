65cf4a3fa3f9c1b17a45164bed5a89554b9da9a9
Prateek Saini <pratiiksaini@gmail.com>
2021-10-02 12:20:06 +0530

    first vite-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
 import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Prateek!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-       
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1>Hello Prateek!</h1>
       </header>
     </div>
   )
